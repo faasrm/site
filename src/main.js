5bfc1db291a8beaeabde7f5390d652232af865ae
faasrm <oofxyt1@gmail.com>
2022-04-13 09:04:18 +0200

    about me

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
 function scrollToOne() {
     document.getElementById('about-me').scrollIntoView();
 }
+
+function scrollToTwo() {
+    document.getElementById('work').scrollIntoView();
+}
+
+function scrollToThree() {
+    document.getElementById('contact').scrollIntoView();
+}
